f7f8029cbe2819d115a2122dc0680998ff0d107d
PANKAJ BHATT <109691325+Pnkajbhatt@users.noreply.github.com>
2026-09-16 18:06:32 +0530

    feat: integrate OpenFeign for inventory management, update Inventory and Order services, and add DTOs for responses

--- ecom-order-service/src/main/java/in/pnkj/ecomorderservice/controller/OrderController.java
+++ ecom-order-service/src/main/java/in/pnkj/ecomorderservice/controller/OrderController.java
@@ -3,7 +3,7 @@ package in.pnkj.ecomorderservice.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +16,8 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
     private final OrderService orderService;
 
-    @GetMapping("/{ProductId}")
-    public ResponseEntity<String> placeOrder(@PathVariable String ProductId) {
+    @PostMapping("/item/{ProductId}")
+    public ResponseEntity<String> placeOrder(@PathVariable(name = "ProductId") Long ProductId) {
         return ResponseEntity.ok(orderService.placeOrder(ProductId));
     }
 
---
+++ ecom-order-service/src/main/java/in/pnkj/ecomorderservice/dto/InventoryDtoResponse.java
@@ -0,0 +1,10 @@
+package in.pnkj.ecomorderservice.dto;
+
+public record InventoryDtoResponse(
+                Long productId,
+                String productName,
+                Long quantity)
+
+{
+        
+}
